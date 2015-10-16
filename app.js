86216d4bedb223b88b38ef749786c78e270f153f
Jeff Meyer <jcmeyer10@gmail.com>
2015-10-16 05:40:30 -0400

    "trying to put the finishing touches on this project"

--- app.js
+++ app.js
@@ -16,16 +16,17 @@ var updateScoreboard, xWinnerMessage, oWinnerMessage, isWinner;
 
 //Begin Document Ready
 $(document).ready(function() {
-  xWin = 0;
-  oWin = 0;
-  player1 = "X";
-  player2 = "O";
-  playerTurn = player1;
-  gamePieces = $('.gameboard').children();
-  winner = 0;
-  token = 0;
-  tie = 0;
+  var xWin = 0;
+  var oWin = 0;
+  var player1 = "X";
+  var player2 = "O";
+  var playerTurn = player1;
+  var gamePieces = $('.gameboard').children();
+  var winner = 0;
+  var token = 0;
+  var tie = 0;
   var game = {};
+  var counter = 0;
 
   var updateScoreboard, xWinnerMessage, oWinnerMessage, isWinner;
 
@@ -48,21 +49,9 @@ $(document).ready(function() {
             ($(gamePieces[2]).text() === player && $(gamePieces[5]).text() ===  player && $(gamePieces[8]).text() ===  player);
   };
 
-var tieGame = function(){
-  for (var i = 0; i < $('.box').length; i++) {
-      if ($('.box').eq(i).html() === "") {
-        return false;
-      }
-    }
-    return "Cat's Game";
-  };
-
-  if (counter === 8){
-    console.log("meow");
-  }
 
 //Click function on the gameboard and it's children so that if there is not a winner, we keep playing and putting in either X or O depending on player turn
-  $(gamePieces).on('click', function(){
+  $(gamePieces).one('click', function(){
     if (!isWinner("X") && !isWinner("O")) {
       $(this).text(playerTurn);
       $('.message').html('');
@@ -70,19 +59,19 @@ var tieGame = function(){
 //Where isWInner function is called and checked after every click
 //adds to win counter if the a winner is found
 //calls updateScoreboard function and updates score accordingly
-    if (isWinner(playerTurn)) {
-      if (counter === 9){
-        console.log("meow")
-      }
-      else if (isWinner("X")) {
+    if (!isWinner(playerTurn)) {
+      if (isWinner("X")) {
         xWin++
         $('.message').html("" + playerTurn + " is the Winner!");
-      } else {
+      } else if (isWinner("O")){
         oWin++;
         $('.message').html("" + playerTurn + " is the Winner!");
+          } else if (counter === 9){
+              console.log("tie")
+            }
           }
     updateScoreboard();
-  }
+  };
 
 //Switches players thus switches between X's and O's
     if (playerTurn === player1){
@@ -94,11 +83,10 @@ var tieGame = function(){
       counter++;
       console.log(counter);
         }
-      }
   });
 
 //Allows players to restart game
-  $('#create-game').on('click', function(){
+  $('#create-game').one('click', function(){
     $(gamePieces).each(function(){
       $(gamePieces).html('');
       $('.message').html("Let's Start a New Game!");
@@ -107,4 +95,3 @@ var tieGame = function(){
 
   });
 });
-
